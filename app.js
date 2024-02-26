15a1e856b7c62ae72ee6d21e7eae9fb9c8d95254
jovani <jovasreyes571@gmail.com>
2024-02-26 10:51:24 -0600

    segundo commit

--- app.js
+++ app.js
@@ -1,112 +1,142 @@
-const Lienzo =document.querySelector('#Lienzo');
-const ctx= Lienzo.getContext('2d');
+const lienzo = document.querySelector('#lienzo');
+const ctx = lienzo.getContext('2d');
 
-ctx.font = "25px Serif";
-ctx.fillText("👻", 100, 100);
 
-const snake=[];
+let posX = 2;
+let posY = 1;
+let direction = 1;
 
-const head= {
-    x:2,
-    y:1,
-    pinta: function(){
-        ctx.font ="25px Serif";
-        ctx.fillText("👻", this.x * 20, this.y *20);
-    }
-}
+const bgSound = new Audio('./Cmbia.mp3');
+const eating = new Audio('./Mordisco.mp3');
+const death = new Audio('./Game_Over.mp3');
 
-snake.push(head);
-snake.push({
+function init(){
+    posX = 2;
+    posY = 1;
+    direction = 1;
+
+    bgSound.play();
+
+    const snake = []
+    ctx.fillStyle = "black";
+    ctx.fillRect(50, 30, 20, 20);
     
-        x:1,
-        y:1,
-        xNext:2,
-        yNext:1,
-
-        pinta: function(){
-            ctx.font ="25px Serif";
-            ctx.fillText("👻", this.x * 20, this.y *20);
-        },
-    },
-)
-snake.push({
+    ctx.font = "25px Serif";
+    ctx.fillText("🤠", 100, 100);
     
-        x:0,
-        y:1,
-        xNext:1,
-        yNext:1,
-
+    const head = {
+        x: 2,
+        y: 1,
         pinta: function(){
-            ctx.font ="25px Serif";
-            ctx.fillText("👻", this.x * 20, this.y *20);
+            ctx.font = "25px Serif";
+            ctx.fillText("👻", this.x * 20, this.y * 20);
         }
-    })
+    }
+    
+    snake.push(head);
+    snake.push({
+        x: 1,
+        y: 1,
+        xNext: 2,
+        yNext: 1,
+        pinta: function(){
+            ctx.font = "25px Serif";
+            ctx.fillText("🤣", this.x * 20, this.y * 20);
+        }
+    });
+    
+    snake.push({
+        x: 0,
+        y: 1,
+        xNext: 1,
+        yNext: 1,
+        pinta: function(){
+            ctx.font = "25px Serif";
+            ctx.fillText("🤣", this.x * 20, this.y * 20);
+        }
+    });
+return snake;    
+}
+let snake = init();
 
 function nextMove(){
     snake.forEach((item, index) =>{
-        if(index ===0){
+        if(index === 0){
             item.x = posX;
             item.y = posY;
-        }else{
+        } else {
             item.x = item.xNext;
             item.y = item.yNext;
-            item.xNext = snake[index -1].x;
-            item.yNext = snake[index -1].y;
+            item.xNext = snake[index - 1].x;
+            item.yNext = snake[index - 1].y;
         }
     })
 }
 
-const food ={
+const food = {
     x: 0,
     y: 0,
     pinta: function(){
-        ctx.fillText('🤣', this.x * 20, this.y * 20);
+        ctx.fillText('😎', this.x * 20, this.y * 20);
     },
     random: function(){
-        this.x = Math.floor(Math.random() * 30);
-        this.y = Math.ceil(Math.random() * 20);
+        this.x = Math.floor(Math.random() * 25);
+        this.y = Math.ceil(Math.random() * 17);
     }
 }
 
 function checkEat(){
-    if (snake[0].x === food.x && snake[0].y === food.y){
-        snake.push({...snake[1]});
+    if(snake[0].x === food.x && snake[0].y === food.y){
+        eating.play();
+        snake.push({ ...snake[1]});
         food.random();
     }
 }
 
 
-let posX = 0;
-let posY = 1;
+function gameOver(){
+    for (let i=1; i<snake.length;i++){
+        if(snake[0].x === snake [i].x && snake[0].y === snake [i].y){
+            return true;
+        }
+    }
+    return false;
+}
 
-let direction = 1;
+
+
 food.random();
-setInterval(()=>{
-    ctx.fillRect(0, 0, 600, 400);
+setInterval(() => {
+    ctx.fillRect(0, 0, 500, 340);
 
+    
     food.pinta();
-
     snake.forEach(item => item.pinta());
 
     checkEat();
+
+    if(gameOver()){
+        death.play();
+        alert('perdiste');
+        snake = init();
+    }
 
     if(direction === 1) posX++;
     else if(direction === 2) posY++;
     else if(direction === 3) posX--;
     else posY--;
-    
-    if(posX > 29) posX = 0;
-    else if(posX <= 0) posX = 30;
-    if(posY > 20) posY = 1;
-    else if(posY < 1) posY = 20;
+
+    if(posX > 24) posX = 0;
+    else if(posX <= 0) posX = 24;
+    if(posY > 17) posY = 1;
+    else if(posY < 1) posY = 17;
 
     nextMove();
 
 }, 200);
 
 document.querySelector('body')
-    .addEventListener('keydown', (e) =>{
-        console.log(e.key)
+    .addEventListener('keydown', (e) => {
         switch(e.key){
             case 'ArrowRight':
                 direction = 1;
@@ -121,26 +151,26 @@ document.querySelector('body')
                 direction = 4;
                 break;
         }
-    });
+    })
 
     document.querySelector('.container')
-        .addEventListener('click',(e) =>{
+        .addEventListener('click',(e) => {
             if(e.target.classList.contains('btn')){
-                const button = e.target.innerText;
+                const button= e.target.innerText;
                 switch(button){
                     case 'Right':
-                        direction = 1;
+                        direction=1;
                         break;
                     case 'Down':
-                        direction = 3;
+                        direction=2;
                         break;
                     case 'Left':
-                        direction = 2;
+                        direction=3;
                         break;
                     case 'Up':
-                        direction = 4;
+                        direction=4;
                         break;
                 }
             }
-            
-        });
+          
+        })
